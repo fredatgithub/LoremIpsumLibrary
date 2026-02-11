b72f60f7b215ba203ae1bc75cf33fc5531d658ef
fred <domaine75@gmail.com>
2026-02-12 00:42:59 +0100

    add unit tests

--- UnitTest.Lorem/UnitTestLipsumType.cs
+++ UnitTest.Lorem/UnitTestLipsumType.cs
@@ -1,14 +1,74 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using LoremIpsumLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace UnitTest.Lorem
 {
   [TestClass]
-  public class UnitTestLorem
+  public class UnitTestLoremIpsumType
   {
     [TestMethod]
-    public void TestMethod1()
+    public void LipsumType_ShouldContainFourValues()
     {
+      var values = Enum.GetValues(typeof(LipsumType));
+      Assert.HasCount(4, values);
+    }
+
+    [TestMethod]
+    public void LipsumType_ShouldContainExpectedNames()
+    {
+      var names = Enum.GetNames(typeof(LipsumType));
+      CollectionAssert.AreEquivalent(new[] { "Words", "Paragraphs", "Bytes", "Lists" }, names);
+    }
+
+    [TestMethod]
+    public void LipsumType_Parse_ShouldReturnCorrectEnum()
+    {
+      var result = (LipsumType)Enum.Parse(typeof(LipsumType), "Words");
+      Assert.AreEqual(LipsumType.Words, result);
+    }
+
+    [TestMethod]
+    public void LipsumType_TryParse_ShouldReturnTrue_ForValidValue()
+    {
+      bool success = Enum.TryParse("Paragraphs", out LipsumType result);
+      Assert.IsTrue(success);
+      Assert.AreEqual(LipsumType.Paragraphs, result);
+    }
+
+    [TestMethod]
+    public void LipsumType_TryParse_ShouldReturnFalse_ForInvalidValue()
+    {
+      bool success = Enum.TryParse("InvalidValue", out LipsumType result);
+      Assert.IsFalse(success);
+    }
+
+    [TestMethod]
+    public void LipsumType_AllDefinedValues_ShouldBeValidEnumValues()
+    {
+      foreach (LipsumType value in Enum.GetValues(typeof(LipsumType)))
+      {
+        Assert.IsTrue(Enum.IsDefined(typeof(LipsumType), value));
+      }
+    }
+
+    [TestMethod]
+    [DataRow("Words", LipsumType.Words)]
+    [DataRow("Paragraphs", LipsumType.Paragraphs)]
+    [DataRow("Bytes", LipsumType.Bytes)]
+    [DataRow("Lists", LipsumType.Lists)]
+    public void LipsumType_TryParse_ShouldMapCorrectly(string input, LipsumType expected)
+    {
+      bool success = Enum.TryParse(input, out LipsumType result);
+      Assert.IsTrue(success);
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void LipsumType_InvalidNumericValue_ShouldNotBeDefined()
+    {
+      const LipsumType invalidValue = (LipsumType)99;
+      Assert.IsFalse(Enum.IsDefined(typeof(LipsumType), invalidValue));
     }
   }
 }
---
+++ UnitTest.Lorem/UnitTestLoremIpsum.cs
@@ -0,0 +1,16 @@
+﻿using LoremIpsumLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Lorem
+{
+  [TestClass]
+  public class UnitTestLoremIpsum
+  {
+    [TestMethod]
+    public void LoremIpsum_GenerateWords_shouldReturnOneWord()
+    {
+      string source = LoremIpsum.CreateText(LipsumType.Words, 1);
+      Assert.IsNotEmpty(source);
+    }
+  }
+}
