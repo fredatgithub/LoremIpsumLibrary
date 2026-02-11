acc20ebdbc9c062bfa5530d3064626a6dfacbeb7
fred <domaine75@gmail.com>
2026-02-11 21:41:18 +0100

    Initial code

---
+++ ExampleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using LoremIpsumLibrary;
+using System;
+
+namespace ExampleApplication
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      string text = LoremIpsum.CreateAllText();
+      Console.WriteLine(text);
+
+      string bigText = LoremIpsum.CreateText(LipsumType.Words, 3);
+      Console.WriteLine(bigText);
+
+      string text1 = LoremIpsum.CreateText(LipsumType.Words, 50);
+      Display(text1);
+      string text2 = LoremIpsum.CreateText(LipsumType.Paragraphs, 3);
+      Display(text2);
+      string text3 = LoremIpsum.CreateText(LipsumType.Lists, 5);
+      Display(text3);
+
+      Console.WriteLine(text1);
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
---
+++ LoremIpsumLibrary/LipsumType.cs
@@ -0,0 +1,13 @@
+﻿namespace LoremIpsumLibrary
+{
+  /// <summary>
+  /// The type of Lipsum to create.
+  /// </summary>
+  public enum LipsumType
+  {
+    Words,
+    Paragraphs,
+    Bytes,
+    Lists
+  }
+}
